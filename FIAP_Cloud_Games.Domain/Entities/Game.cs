c4aefab7722c2ebb1c54d7cb0aeeb08b28bfab2c
Henrique Faria <henriquerbf@hotmail.com>
2025-10-03 01:07:36 -0300

    Create Game.cs

---
+++ FIAP_Cloud_Games.Domain/Entities/Game.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIAP_Cloud_Games.Domain.Entities
+{
+    public class Game
+    {
+        // Properties
+        public Guid Id { get; private set; }
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+        public string Genre { get; private set; }
+        public DateTime ReleaseDate { get; private set; }
+        public decimal Price { get; private set; }
+        public decimal Discount { get; private set; }
+        public decimal SalePrice => Price - (Price * Discount);
+
+        // Constructor required by EF Core (reflection)
+        private Game() { }
+
+        // Constructor used when creating new instances
+        public Game(string title, string description, string genre, decimal price, DateTime releaseDate)
+        {
+            Title = title;
+            Description = description;
+            Genre = genre;
+            Price = price;
+            ReleaseDate = releaseDate;
+        }
+
+    }
+}
