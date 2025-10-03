cd0ff8985b030d397cf55a6813876b081a21db7d
Henrique Faria <henriquerbf@hotmail.com>
2025-10-03 01:07:25 -0300

    Add Class User

---
+++ FIAP_Cloud_Games.Domain/Entities/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIAP_Cloud_Games.Domain.Entities
+{
+    public class User
+    {
+        // Properties
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public string Role { get; private set; }
+        public string Email { get; private set; }
+        public string Password { get; private set; }
+        public DateTime CreatedDate { get; private set; } = DateTime.Now;
+
+        // Constructors
+        private User() { }
+
+        // Constructor used when creating new instances
+        public User(string name, string tole, string email, string password)
+        {
+            Name = name;
+            Role = tole;
+            Email = email;
+            Password = password;
+        }
+    }
+}
