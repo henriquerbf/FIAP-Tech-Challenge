b0319fa49c17ad7dda1a6b8929d2534daa87e59a
Henrique Faria <henriquerbf@hotmail.com>
2025-10-22 19:51:34 -0300

    User Guard Methods

--- FIAP_Cloud_Games.Domain/Entities/User.cs
+++ FIAP_Cloud_Games.Domain/Entities/User.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace FIAP_Cloud_Games.Domain.Entities
+﻿namespace FIAP_Cloud_Games.Domain.Entities
 {
+    /// <summary>
+    /// Represents a system user (client or administrator).
+    /// </summary>
     public class User
     {
         // Properties
@@ -15,17 +12,57 @@ namespace FIAP_Cloud_Games.Domain.Entities
         public string Email { get; private set; }
         public string Password { get; private set; }
         public DateTime CreatedDate { get; private set; } = DateTime.Now;
+        public List<Game> Library { get; private set; }
 
         // Constructors
         private User() { }
 
         // Constructor used when creating new instances
-        public User(string name, string tole, string email, string password)
+        public User(string name, string role, string email, string password)
         {
+            UpdateName(name);
+            UpdateEmail(email);
+            ChangePassword(password);
+            AssignRole(role);
+
+            Id = Guid.NewGuid();
+            Library = new List<Game>();
+        }
+
+        // Guard Methods
+        public void UpdateName(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException("Invalid name.");
             Name = name;
-            Role = tole;
+        }
+
+        public void UpdateEmail(string email)
+        {
+            if (string.IsNullOrWhiteSpace(email) || !email.Contains("@"))
+                throw new ArgumentException("Invalid email.");
             Email = email;
+        }
+
+        // Business Methods
+        public void ChangePassword(string password)
+        {
+            if (string.IsNullOrWhiteSpace(password))
+                throw new ArgumentException("Invalid password hash.");
             Password = password;
+        }
+
+        public void AssignRole(string role)
+        {
+            if (role != "User" && role != "Admin")
+                throw new ArgumentException("Invalid role.");
+            Role = role;
+        }
+
+        public void AcquireGame(Game game)
+        {
+            if (!Library.Any(g => g.Id == game.Id))
+                Library.Add(game);
         }
     }
 }
