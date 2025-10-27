ce04655f345282c95046e00f372bab770bf81be2
Fillipy Saraiva <lip-crs@hotmail.com>
2025-10-26 21:43:43 -0300

    Create UserGame.cs
    adding UserGame entity to represent user-game relationship

---
+++ FIAP_Cloud_Games.Domain/Entities/UserGame.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIAP_Cloud_Games.Domain.Entities
+{
+    public class UserGame
+    {
+        public Guid UserId { get; set; }
+        public Guid GameId { get; set; }
+        public DateTime AddedAt { get; set; } = DateTime.UtcNow;
+
+        public User User { get; set; } = default!;
+        public Game Game { get; set; } = default!;
+    }
+}
