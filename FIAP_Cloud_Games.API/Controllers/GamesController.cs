fb4e7cc8b9f7c31d565dfb1d03aedb258a09eeda
Henrique Faria <henriquerbf@hotmail.com>
2025-10-30 01:39:51 -0300

    Update GamesController.cs

--- FIAP_Cloud_Games.API/Controllers/GamesController.cs
+++ FIAP_Cloud_Games.API/Controllers/GamesController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FIAP_Cloud_Games.Domain.Entities;
+using FIAP_Cloud_Games.Infrastructure.Persistence.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -8,36 +11,75 @@ namespace FIAP_Cloud_Games.Controllers
     [ApiController]
     public class GamesController : ControllerBase
     {
+        private readonly CloudGamesDbContext _context;
+        public GamesController(CloudGamesDbContext context) => _context = context;
+
         // GET: api/<GamesController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public async Task<ActionResult<IEnumerable<Game>>> Get()
         {
-            return new string[] { "value1", "value2" };
+            // usa o _context normalmente
+            return await _context.Games.AsNoTracking().ToListAsync();
         }
 
         // GET api/<GamesController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public async Task<ActionResult<Game>> Get(Guid id)
         {
-            return "value";
+            return await _context.Games.FindAsync(id);
         }
 
         // POST api/<GamesController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<ActionResult<Game>> AddGame(Game Game)
         {
+            _context.Games.Add(Game);
+            await _context.SaveChangesAsync(); // executa o INSERT
+
+            return CreatedAtAction(nameof(Get), new { id = Game.Id }, Game);
         }
 
         // PUT api/<GamesController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public async Task<IActionResult> UpdateGame(Guid id, Game Game)
         {
+            if (id != Game.Id)
+                return BadRequest("O ID da URL não corresponde ao do corpo da requisição.");
+
+            _context.Entry(Game).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync(); // executa o UPDATE
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!GameExists(id))
+                    return NotFound(); // se o usuário não existe mais
+                else
+                    throw; // re-lança se for outro tipo de erro
+            }
+
+            return NoContent(); // 204
         }
 
         // DELETE api/<GamesController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public async Task<IActionResult> DeleteGame(int id)
         {
+            var Game = await _context.Games.FindAsync(id);
+            if (Game == null)
+                return NotFound();
+
+            _context.Games.Remove(Game);
+            await _context.SaveChangesAsync(); // executa o DELETE
+
+            return NoContent();
+        }
+
+        private bool GameExists(Guid id)
+        {
+            return _context.Games.Any(e => e.Id == id);
         }
     }
 }
