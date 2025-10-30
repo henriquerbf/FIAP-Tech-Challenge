a9dffa8fda3951a96a886bb9ca29572548bba515
Henrique Faria <henriquerbf@hotmail.com>
2025-10-30 01:39:53 -0300

    Update UsersController.cs

--- FIAP_Cloud_Games.API/Controllers/UsersController.cs
+++ FIAP_Cloud_Games.API/Controllers/UsersController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FIAP_Cloud_Games.Domain.Entities;
+using FIAP_Cloud_Games.Infrastructure.Persistence.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -8,36 +11,75 @@ namespace FIAP_Cloud_Games.Controllers
     [ApiController]
     public class UsersController : ControllerBase
     {
+        private readonly CloudGamesDbContext _context;
+        public UsersController(CloudGamesDbContext context) => _context = context;
+
         // GET: api/<UsersController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public async Task<ActionResult<IEnumerable<User>>> Get()
         {
-            return new string[] { "value1", "value2" };
+            // usa o _context normalmente
+            return await _context.Users.AsNoTracking().ToListAsync();
         }
 
         // GET api/<UsersController>/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public async Task<ActionResult<User>> Get(Guid id)
         {
-            return "value";
+            return await _context.Users.FindAsync(id);
         }
 
         // POST api/<UsersController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<ActionResult<User>> AddUser(User user)
         {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync(); // executa o INSERT
+
+            return CreatedAtAction(nameof(Get), new { id = user.Id }, user);
         }
 
         // PUT api/<UsersController>/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public async Task<IActionResult> UpdateUser(Guid id, User user)
         {
+            if (id != user.Id)
+                return BadRequest("O ID da URL não corresponde ao do corpo da requisição.");
+
+            _context.Entry(user).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync(); // executa o UPDATE
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserExists(id))
+                    return NotFound(); // se o usuário não existe mais
+                else
+                    throw; // re-lança se for outro tipo de erro
+            }
+
+            return NoContent(); // 204
         }
 
         // DELETE api/<UsersController>/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public async Task<IActionResult> DeleteUser(int id)
         {
+            var user = await _context.Users.FindAsync(id);
+            if (user == null)
+                return NotFound();
+
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync(); // executa o DELETE
+
+            return NoContent();
+        }
+
+        private bool UserExists(Guid id)
+        {
+            return _context.Users.Any(e => e.Id == id);
         }
     }
 }
